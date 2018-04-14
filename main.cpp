0f95ea7c07650dbc2c2fae5a9e778592ae8ae396
bugdie4k <fedorough@gmail.com>
2018-04-14 15:55:19 +0300

    ok, im getting somewhere

--- main.cpp
+++ main.cpp
@@ -54,14 +54,15 @@ class Chromosome {
 
      int size();
      void pprint();
-     bool operator < (const Chromosome &other);
-     bool operator > (const Chromosome &other);
+     friend bool operator < (const Chromosome& l, const Chromosome& r);
+     friend bool operator > (const Chromosome& l, const Chromosome& r);
      vector<double>::iterator begin();
      vector<double>::iterator end();
      void push(double arg);
 };
 
 void Chromosome::push(double arg) {
+    // cout << "    pushing arg to chromosome: " << arg << endl;
     this->argv.push_back(arg);
 }
 
@@ -73,12 +74,12 @@ vector<double>::iterator Chromosome::end() {
     return this->argv.end();
 }
 
-bool Chromosome::operator < (const Chromosome &other) {
-    return this->res < other.res;
+bool operator < (const Chromosome& l, const Chromosome& r) {
+    return l.res < r.res;
 }
 
-bool Chromosome::operator > (const Chromosome &other) {
-    return this->res > other.res;
+bool operator > (const Chromosome& l, const Chromosome& r) {
+    return l.res > r.res;
 }
 
 std::ostream &operator << (std::ostream &os, Chromosome &obj) {
@@ -116,11 +117,13 @@ class Population {
     public:
      vector<Chromosome*> pop;
      double (*fn)(vector<double>);
+     double from;
+     double to;
 
-     explicit Population(double (*fn)(vector<double>));
+     explicit Population(double (*fn)(vector<double>), double from, double to);
 
      void set(vector<Chromosome*>);
-     void randomize(int n, int argn, double from, double to);
+     void randomize(int n, int argn);
      void iterate();
 
      void pprint();
@@ -133,18 +136,20 @@ class Population {
      vector<Chromosome*> mutate(vector<Chromosome*> crossed);
 };
 
-Population::Population(double (*fn)(vector<double>)) {
+Population::Population(double (*fn)(vector<double>), double from, double to) {
     this->fn = fn;
+    this->from = from;
+    this->to = to;
 }
 
 void Population::set(vector<Chromosome*> pop) {
     this->pop = pop;
 }
 
-void Population::randomize(int n, int argn, double from, double to) {
+void Population::randomize(int n, int argn) {
     for (int i = 0; i < n; ++i) {
         Chromosome* ch = new Chromosome();
-        ch->randomize(argn, from, to, this->fn);
+        ch->randomize(argn, this->from, this->to, this->fn);
         pop.push_back(ch);
     }
 }
@@ -190,42 +195,69 @@ double fn1(vector<double> argv) {
 }
 
 array<Chromosome*, 2> Population::cross2(Chromosome* c1, Chromosome* c2) {
+    cout << "  - crossing 2" << endl;
+    cout << "  " << c1 << "  " << c2;
+
     int argn1 = c1->size();
     int argn2 = c2->size();
     assert(argn1 == argn2);
     array<Chromosome*, 3> crossed;
-    auto it1 = c1->begin();
-    auto it2 = c2->begin();
-    for ( ; it1 != c1->end() && it2 != c2->end(); ++it1, ++it2) {
-        double x1 = *it1;
-        double x2 = *it2;
-        crossed[0]->push((x1 + x2) / 2);
-        crossed[1]->push((3 * x1 - x2) / 2);
-        crossed[2]->push((- x1 + 3 * x2) / 2);
+    for (auto c = crossed.begin(); c != crossed.end(); ++c)
+        (*c) = new Chromosome();
+
+    for (int i = 0; i < argn1; ++i) {
+        double x1 = c1->argv[i];
+        double x2 = c2->argv[i];
+        double crossed_arg1 = (x1 + x2) / 2;
+        double crossed_arg2 = (3 * x1 - x2) / 2;
+        double crossed_arg3 = (- x1 + 3 * x2) / 2;
+
+        cout << "    - crossing arguments " << i + 1 << ": " << x1 << ", " << x2 << " -> "
+             << crossed_arg1 << ", " << crossed_arg2 << ", " << crossed_arg3 << endl;
+
+        crossed[0]->push(crossed_arg1);
+        crossed[1]->push(crossed_arg2);
+        crossed[2]->push(crossed_arg3);
     }
-    // assert(crossed[0].size() == argn1)
+    // fill the "res" field
     for (auto c = crossed.begin(); c != crossed.end(); ++c)
         (*c)->res = this->fn((*c)->argv);
-    std::sort(crossed.begin(), crossed.end());
-    array<Chromosome*, 2> selected_crossed = { crossed[2], crossed[1] };
+
+    cout << "  - 3 children of 2 crossed:" << endl;
+    for (auto c = crossed.begin(); c != crossed.end(); ++c)
+        cout << "  " << *c;
+
+    std::sort(crossed.begin(), crossed.end(),
+              [](Chromosome* c1, Chromosome* c2) -> bool {
+                  return *c1 > *c2;
+              });
+
+    // cout << "  3 sorted children of 2 crossed:" << endl;
+    // for (auto c = crossed.begin(); c != crossed.end(); ++c)
+    //     cout << "  " << *c;
+
+    array<Chromosome*, 2> selected_crossed = { crossed[0], crossed[1] };
+
+    cout << "  - leaving:" << endl << "  " << selected_crossed[0] << "  " << selected_crossed[1] << endl;
+
     return selected_crossed;
 }
 
 vector<Chromosome*> Population::crossover(vector<Chromosome*> parents) {
-    cout << "selected" << endl;
+    cout << "- selected" << endl;
     pprint_v(parents);
-    vector<Chromosome*> new_pop;
+    vector<Chromosome*> new_pop_v;
     int size = parents.size();
     for (int i = 0; i < size; ++i) {
         Chromosome* c1 = parents[random_i(0, size - 1)];
         Chromosome* c2 = parents[random_i(0, size - 1)];
         array<Chromosome*, 2> crossed = cross2(c1, c2);
-        new_pop.push_back(crossed[0]);
-        new_pop.push_back(crossed[1]);
+        new_pop_v.push_back(crossed[0]);
+        new_pop_v.push_back(crossed[1]);
     }
-    cout << "new_population" << endl;
-    pprint_v(new_pop);
-    return new_pop;
+    cout << "- new_population" << endl;
+    pprint_v(new_pop_v);
+    return new_pop_v;
 }
 
 vector<Chromosome*> Population::mutate(vector<Chromosome*> pop) {
@@ -233,15 +265,15 @@ vector<Chromosome*> Population::mutate(vector<Chromosome*> pop) {
 }
 
 void Population::iterate() {
-    vector<Chromosome*> new_pop = this->mutate(this->crossover(this->select()));
-    this->pop = new_pop;
+    vector<Chromosome*> new_pop_v = this->mutate(this->crossover(this->select()));
+    this->pop = new_pop_v;
 }
 
 int main(int argc, char** argv) {
-    Population* pop = new Population(fn1);
-    pop->randomize(N, 2, -2.048, 2.048);
+    Population* pop = new Population(fn1, -2.048, 2.048);
+    pop->randomize(N, 2);
 
-    cout << "random ini" << endl;
+    cout << "- random ini" << endl;
     pop->pprint();
 
     pop->iterate();
